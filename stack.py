447c39d40cd12c6ef11bce04930bbb5142e48544
Antonio Kam <19kamh3@gmail.com>
2020-04-28 11:49:13 +0800

    add to queue

--- stack.py
+++ stack.py
@@ -1,12 +1,12 @@
 # In this file, we assume that a list created is a stack, where the only operations that can be done would be to push to the end of the list and to pop from the end of the list.
 
-stack = [1,2,3,4,5,6,7,8]
+stack = []
 
-def add_to_stack(for_insert, s):
+def add_node(for_insert, s):
     stack = s.copy() # Makes a copy of the stack (for mutability purposes)
     stack.append(for_insert)
 
-def remove_from_stack(s):
+def delete_node(s):
     stack = s.copy()
     stack.pop()
 
@@ -18,7 +18,8 @@ def traverse_until_match(to_find, s):
     print("not found")
 
 
-def traverse_whole_list(s):
+def traverse_whole(s):
     stack = s.copy()
     while len(stack) != 0: # Checks to see if the stack is empty
         print(stack.pop())
+    print("not found")
